c27fa5e2259712e90239ec55166ba184b06d389c
mtimmer73 <timmermann.matthew@yahoo.com>
2026-04-26 22:22:03 -0400

    Update push_outputs.py

--- push_outputs.py
+++ push_outputs.py
@@ -1,8 +1,22 @@
+import os
 import subprocess
 
-subprocess.run(["git", "config", "--global", "user.email", "prophit-bot@auto.com"])
-subprocess.run(["git", "config", "--global", "user.name", "prophit-bot"])
+username = os.environ["GITHUB_USERNAME"]
+token = os.environ["GITHUB_TOKEN"]
+repo = "prophit"
+branch = "main"
 
-subprocess.run(["git", "add", "proppulse-ui-app/public/data/prophit_latest.json"])
-subprocess.run(["git", "commit", "-m", "auto update"])
-subprocess.run(["git", "push"])
+remote_url = f"https://{username}:{token}@github.com/{username}/{repo}.git"
+
+subprocess.run(["git", "config", "--global", "user.email", "prophit-bot@example.com"], check=True)
+subprocess.run(["git", "config", "--global", "user.name", "prophit-bot"], check=True)
+
+subprocess.run(["git", "remote", "remove", "origin"], check=False)
+subprocess.run(["git", "remote", "add", "origin", remote_url], check=True)
+
+subprocess.run(["git", "add", "data/prophit_latest.json"], check=True)
+subprocess.run(["git", "commit", "-m", "Auto-update PropHit data"], check=False)
+
+subprocess.run(["git", "push", "origin", f"HEAD:{branch}"], check=True)
+
+print("PropHit JSON pushed to GitHub")
